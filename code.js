ec595ba5d223d9973a1a1a54a68a59361ba376f5
pwieleba <piotrwieleba8@gmail.com>
2022-07-19 21:32:12 +0200

    first functions attempt
    failed

--- code.js
+++ code.js
@@ -1,2 +1,12 @@
-
-console.log("food")
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn btn-danger')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+}
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
